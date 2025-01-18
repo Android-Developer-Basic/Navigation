3cf3df5564d457dd1f49bc0ee0977b66d94ed1ac
SpiritualAdviser <pomaxam@narod.ru>
2025-01-18 15:06:31 +0300

    add AlertDialogDialogFragment

---
+++ app/src/main/kotlin/ru/otus/cookbook/cook_recycler_view/AlertDialogDialogFragment.kt
@@ -0,0 +1,16 @@
+package ru.otus.cookbook.cook_recycler_view
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class AlertDialogDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = activity?.let { AlertDialog.Builder(it) }
+        builder?.setTitle("title")
+        builder?.setMessage("masage")
+        return builder?.create()!!
+    }
+}
